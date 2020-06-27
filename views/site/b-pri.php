7a6bcb17e023a6b0509453cd3a7ad77e9f57d7e5
pavel <kimpauevg@mail.ru>
2020-06-27 16:28:08 +0700

    add all bachelor programs

---
+++ views/site/b-pri.php
@@ -0,0 +1,433 @@
+
+<section class="section-welcome">
+    <div class="background"></div>
+    <div class="section-content">
+        <div class="container pb-5">
+            <h1  >Программная инженерия</h1>
+            <div class="container pill-container pb-4 wow fadeInDown">
+                <div class="badge-pill badge-secondary">
+                    <div class="pill-text">
+                        Бакалавриат
+                    </div>
+                </div>
+                <div class="badge-pill badge-secondary pill-multirow">
+                    <div class="pill-text">
+                        09.03.04 – Программная инженерия
+                    </div>
+                </div>
+            </div>
+            <p>
+                Программная инженерия является одним из новых направлений подготовки IT-
+                специалистов в России. Выпускники направления владеют методами и
+                инструментальными средствами управления программными проектами,
+                проектирования, разработки, тестирования и внедрения программных систем,
+                свободно владеют знаниями и навыками предпринимательства и ведения бизнеса.<br/>
+                Благодаря уникальному сочетанию знаний в области компьютерных технологий,
+                программирования и проектирования информационных систем, управления
+                программными проектами, выпускники становятся самыми востребованными
+                специалистами на рынке труда не только в России, но и за рубежом.
+            </p>
+        </div>
+        <div class="container">
+            <div class="row flex-column flex-sm-row">
+                <div class="col-lg-4 col-12 course-item-holder">
+                    <div class="col-auto1">
+                        <div class=" course-item wow fadeInUp ">
+
+                            <div class="course-item__label">Места</div>
+                            <div class="course-item__value">? бюджетных мест<br><p></p></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-12 course-item-holder">
+                    <div class="col-auto1">
+                        <div class="course-item wow fadeInUp">
+
+                            <div class="course-item__label">форма обучения</div>
+                            <div class="course-item__value">? </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-12 course-item-holder">
+                    <div class="col-auto1">
+                        <div class="course-item wow fadeInUp ">
+
+                            <div class="course-item__label">Проходной балл </div>
+                            <div class="course-item__value">2019 год – ?<br>2018 год – ?</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row flex-column flex-sm-row ">
+                <div class="course-item wow fadeInUp col-lg-4 col-12 course-item-holder">
+                    <div class="course-item-benefit">
+                        <div class="img-tick"></div>
+                        <div>Общежитие</div>
+                    </div>
+                </div>
+                <div class="course-item wow fadeInUp col-lg-4 col-12 course-item-holder"  >
+                    <div class="course-item-benefit">
+                        <div class="img-tick"></div> Военная кафедра
+                    </div>
+                </div>
+                <div class="course-item wow fadeInUp col-lg-4 col-12 course-item-holder"  >
+                    <div class="course-item-benefit">
+                        <div class="img-tick"></div>  Электронное обучение
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--форма подать заявку-->
+<section class="section-request">
+    <div class="section-content">
+        <div class="container">
+            <div class="grid-container">
+                <div class="grid-instruction-row">
+                    <div class="grid-container-item grid-instruction-bold">
+                        Вы можете подать заявление на обучение у нас уже сейчас
+                    </div>
+                    <div class="grid-container-item">
+                        <a href="https://apply.tpu.ru/" class="btn btn-danger btn-lg">Подать заявление!</a>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!--цель-->
+<section class="section-competencies">
+    <div class="section-content">
+        <div class="container">
+            <div class="program-holder">
+                <div class="program-target">
+                    <div class="headline"> Цель программы </div>
+                    <p class="text">
+                        Программа обучения направлена на подготовку специалистов в области полного цикла
+                        производства программного обеспечения – от начальных стадий проектирования
+                        до внедрения и сопровождения готовых программных и информационных систем.
+                    </p>
+                </div>
+            </div>
+            <hr class="horizontal-line">
+        </div>
+    </div>
+</section>
+
+<!--компетанции-->
+<section class="section-benefits ">
+    <div class="white-holder">
+        <div class="headline">
+            Программа нацелена на компетенции
+        </div>
+        <div class="section-content">
+            <div class="container">
+                <div class="row">
+                    <div class=" col-sm-4"">
+                    <div class="row">
+                        <div class="benefit-item benefit-graph col-3"></div>
+                        <div class="benefit-title col-8">Цель программы</div>
+                    </div>
+                    <div class="benefit-text">Практико-ориентированная подготовка специалистов, профессионально владеющих технологиями.</div>
+                </div>
+                <div class=" col-sm-4">
+                    <div class="row">
+                        <div class="benefit-item benefit-programming col-3"> </div>
+                        <div class="benefit-title col-8">Формат обучения</div>
+                    </div>
+                    <div class="benefit-text">Использование разнообразных ресурсов и стилей обучения, учитывающих постоянно изменяющуюся среду для ведения.</div>
+                </div>
+                <div class=" col-sm-4">
+                    <div class="row">
+                        <div class="benefit-item benefit-task col-3" >  </div>
+                        <div class="benefit-title col-8">Диплом </div>
+                    </div>
+                    <div class="benefit-text">Присваивается степень магистра по направлению Информатика и вычислительная техника.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+
+<!--баллы егэ-->
+<section class="section-why" style="background-color:#f5f5f5">
+    <div class="section-content">
+        <div class="container" >
+            <div class="row flex-column flex-lg-row justify-content-start justify-content-lg-between">
+                <div class="col-auto left-block">
+                    <div class="section-title wow fadeInUp"  style="color:#776c7e;" >Минимальный проходной балл ЕГЭ </div>
+                    <div class="section-text row">
+                        <div class="col-sm-4 col-12">
+                            <div class="circle " >
+                                <div class="circle-title">?</div>
+                                <div class="">математика</div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 col-12">
+                            <div class="circle">
+                                <div class="circle-title">?</div>
+                                <div class="">информатика </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 col-12">
+                            <div class="circle">
+                                <div class="circle-title">?</div>
+                                <div class="">русский </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-auto right-block">
+                    <div class="statistic-block" >
+                        <div class="stat-title">?</div>
+                        <div class="stat-text">средний балл <br>поступивших на<br> бюджетную форму<br> в 2019 году
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--дисциплины-->
+<section class="frequent-questions">
+    <div class="white-holder">
+        <div class="questions-container">
+            <div class="headline">
+                Дисциплины бакалаврвской программы
+            </div>
+            <div class="nav-container">
+                <?= \app\helpers\RenderHelper::renderPrograms([
+                    'Архитектура вычислительных систем',
+                    'Операционные системы и сети',
+                    'Базы данных',
+                    'Конструирование программного обеспечения',
+                    'Проектирование и архитектура программных систем',
+                    'Тестирование программного обеспечения',
+                    'Проектирование Интернет-приложений',
+                    'Технология командной разработки программного обеспечения',
+                    'Проектирование информационных систем',
+                ])?>
+            </div>
+            <!--            <div class="nav-container">-->
+            <!--                <div class="gradient-navbar navbar-bakalavriat navbar-left">-->
+            <!--                    <div class="choices">-->
+            <!--                        <button class="choice-wrap chosen" data-index="1">-->
+            <!--                            <div class="choice">-->
+            <!--                                Есть ли что-то-->
+            <!--                            </div>-->
+            <!--                        </button>-->
+            <!--                        <div class="choice-wrap-after"></div>-->
+            <!--                        <button class="choice-wrap" data-index="2">-->
+            <!--                            <div class="choice">-->
+            <!--                                Есть ли что-то 2-->
+            <!--                            </div>-->
+            <!---->
+            <!--                        </button>-->
+            <!--                        <div class="choice-wrap-after"></div>-->
+            <!---->
+            <!--                        <button class="choice-wrap" data-index="3">-->
+            <!--                            <div class="choice">-->
+            <!--                                Есть ли что-то 3-->
+            <!--                            </div>-->
+            <!---->
+            <!--                        </button>-->
+            <!--                        <div class="choice-wrap-after"></div>-->
+            <!---->
+            <!--                        <button class="choice-wrap" data-index="4">-->
+            <!--                            <div class="choice">-->
+            <!--                                Есть ли что-то 4-->
+            <!--                            </div>-->
+            <!---->
+            <!--                        </button>-->
+            <!--                        <div class="choice-wrap-after"></div>-->
+            <!--                        <div class="arrow-holder">-->
+            <!--                            <div class="arrow-down">-->
+            <!---->
+            <!--                            </div>-->
+            <!---->
+            <!--                        </div>-->
+            <!--                    </div>-->
+            <!--                </div>-->
+            <!---->
+            <!--                <div class="answers">-->
+            <!--                    <div class="answer visible" data-index="1">-->
+            <!--                        Ответ-->
+            <!--                    </div>-->
+            <!--                    <div class="answer" data-index="2">-->
+            <!--                        Ответ 2-->
+            <!--                    </div>-->
+            <!--                    <div class="answer" data-index="3">-->
+            <!--                        Ответ 3-->
+            <!--                    </div>-->
+            <!--                    <div class="answer" data-index="4">-->
+            <!--                        Ответ 4-->
+            <!--                    </div>-->
+            <!---->
+            <!--                </div>-->
+            <!---->
+            <!--            </div>-->
+
+        </div>
+    </div>
+</section>
+<!--вакансии-->
+<section class="section-why" style="background-color:#f5f5f5">
+    <div class="section-content">
+        <div class="container" >
+            <div class="row flex-column flex-lg-row justify-content-start justify-content-lg-between">
+                <div class="col-auto left-block">
+                    <div class="section-title wow fadeInUp"  style="color:#b87571">Наши студенты работают в </div>
+                    <div class="section-text">
+                        ОАО «ТПО «Контур», ФГУП «Томский электротехнический завод», ОАО
+                        «Сибэлектромотор», ООО «Завод приборных подшипников», НИИ Электронных
+                        систем ТУСУР, ФГУП НПЦ «Полюс», ОАО «Манометр», ООО «Газпром трансгаз
+                        Томск», в ООО «Томская электронная компания», ОАО «ТомскНИПИнефть ВНК»,
+                        ЦНИИ робототехники и технической кибернетики (г. Санкт-Петербург).<br/>
+                        Должности: инженер, инженер-конструктор, инженер-проектировщик.
+                    </div>
+                </div>
+                <div class="col-auto right-block">
+                    <div class="statistic-block">
+                        <div class="stat-title">?</div>
+                        <div class="stat-text">рублей средняя зарплата <br /> в России <br /> (по данным hh.ru)</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--TODO remove -->
+<!--<section class="section-why" style="background-color:#f5f5f5">-->
+<!--    <div class="section-content">-->
+<!--        <div class="container" >-->
+<!--            <div class="row flex-column flex-lg-row justify-content-start justify-content-lg-between">-->
+<!--                <div class="col-auto left-block">-->
+<!--                    <div class="section-title wow fadeInUp"  style="color:#b87571">Пример с промежутком </div>-->
+<!--                    <div class="section-text">-->
+<!--                        Это потом нужно удалить-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="col-auto right-block">-->
+<!--                    <div class="statistic-block gap-block">-->
+<!--                        <div class="stat-title">≈ 80 - 90 000</div>-->
+<!--                        <div class="stat-text">рублей средняя зарплата <br />набизнес-консультанта<br />  в России <br /> (по данным hh.ru)</div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</section>-->
+
+
+<!--преподаватели-->
+<section class="lecturers">
+    <div class="light-grey-holder1">
+        <div class="headline">
+            Преподаватели и эксперты
+        </div>
+        Отсутствуют фотографии
+
+    </div>
+</section>
+<!--темы работ-->
+<section class="qualification-topics">
+    <div class="white-holder">
+        <div class="headline">
+            Темы выпускных<br> квалификационных работ
+        </div>
+        <div class="section-content">
+            <div class="container">
+                <?= \app\helpers\RenderHelper::renderQualificationWorkThemes([
+                    'Проектирование и разработка мобильного android-приложения для оценки навыков соискателя на должность тестировщика программного обеспечения.',
+                    'Разработка веб-приложения для оптимизации процесса командной разработки ПО.',
+                    'Разработка автоматизированных тестов ИПС Центрального банка РФ.',
+                    'Разработка компонентов мобильной ГИС.',
+                    'Разработка платформы психологического тестирования для корпоративных клиентов.',
+                ])?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--отзывы-->
+<section class="reviews">
+    <div class="light-grey-holder1">
+        <div class="headline">
+            Отзывы наших выпускников
+        </div>
+        Отсутствуют фотографии
+    </div>
+</section>
+
+<section class="section-request">
+    <div class="section-content">
+        <div class="container">
+            <div class="grid-container">
+                <div class="grid-instruction-row">
+                    <div class="grid-container-item grid-instruction-bold">
+                        Вы можете подать заявление на обучение у нас уже сейчас
+                    </div>
+                    <div class="grid-container-item">
+                        <a href="https://apply.tpu.ru/" class="btn btn-danger btn-lg">Подать заявление!</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+<!--другие программы-->
+<section class="courses">
+    <div class="section-content">
+        <div class="light-grey-holder">
+            <div class="headline">
+                Другие направления бакалавриата
+            </div>
+
+
+            <div class="container-wide">
+                <div class="programs-list row">
+                    <a href="/batp" class="program-item">
+                        <div class="program-subject">
+                            15.03.04
+                        </div>
+                        <div class="program-title">Автоматизация технологических процессов и производств</div>
+                        <div class="program-sep"></div>
+                    </a>
+                    <a href="/bdes" class="program-item">
+                        <div class="program-subject">
+                            54.03.01
+                        </div>
+                        <div class="program-title">Дизайн</div>
+                    </a>
+                    <a href="/bivt" class="program-item">
+                        <div class="program-subject">
+                            09.03.01
+                        </div>
+                        <div class="program-title">Информатика и вычислительная техника</div>
+                    </a>
+
+                    <a href="/bmir" class="program-item">
+                        <div class="program-subject">
+                            15.03.06
+                        </div>
+                        <div class="program-title">Мехатроника и робототехника</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
